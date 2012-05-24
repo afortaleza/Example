37e68834d044407871b286e9a2bf5dfd3a638a08
Anderson Fortaleza <afortaleza@gmail.com>
2012-05-23 21:48:48 -0300

    Full project files

--- pages/config_page.php
+++ pages/config_page.php
@@ -1,13 +1,33 @@
-<form action="<?php echo plugin_page('config_update') ?>" method="post">
-    <?php echo form_security_field('plugin_Example_config_update') ?>
+<?php
+    html_page_top( plugin_lang_get('configuration') );
+    $t_foo_or_bar = plugin_config_get('foo_or_bar');
+?>
 
-    <label>Foo or Bar?<br/>
-        <input name="foo_or_bar" value="<?php plugin_config_get('foo_or_bar') ?>"/>
-    </label>
-    <br/>
-    <label>
-        <input type="checkbox" name="reset"/> Reset
-    </label>
-    <br/>
-    <input type="submit"/>
+<br/>
+
+<form action="<?php echo plugin_page( 'config_update' ) ?>" method="post">
+    <?php echo form_security_field( 'plugin_Example_config_update' ) ?>
+    <table class="width60" align="center">
+        <tr>
+            <td class="form-title" rowspan="2"><?php echo plugin_lang_get( 'configuration' ) ?></td>
+        </tr>
+
+        <tr <?php echo helper_alternate_class() ?>>
+            <td class="category"><php echo plugin_lang_get('foo_or_bar') ?></td>
+            <td><input name="foo_or_bar" value="<?php echo string_attribute($t_foo_or_bar) ?>"/></td>
+        </tr>
+
+        <tr <?php echo helper_alternate_class() ?>>
+            <td class="category"><php echo plugin_lang_get( 'reset' ) ?></td>
+            <td><input type="checkbox" name="reset"/></td>
+        </tr>
+
+        <tr>
+            <td class="center" rowspan="2"><input type="submit"/></td>
+        </tr>
+    </table>
 </form>
+
+<?php
+    html_page_bottom();
+?>
